33af63aafaa9b39b57d3b519d81bfd1de503a7ce
github wubuchako <jfdev12@gmail.com>
2020-08-09 16:14:55 +0900

    twitter function

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 //Get quote from API
 async function getQuote() {
   const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,12 +11,37 @@ async function getQuote() {
   try {
     const response = await fetch(proxyUrl + apiUrl);
     const data = await response.json();
-    console.log(data);
+    //If Author is blank, add 'Unknown'
+    if(data.quoteAuthor === ''){
+        authorText.innerText = 'Unknown';
+    } else {
+        authorText.innerText = data.quoteAuthor;
+    }
+    
+    //Reduce font size for long quotes
+    if(data.quoteText.length > 50){
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error){
       getQuote();
-      console.log('whoops, no quote', error);
+     
   }
 }
+
+//Tweet Quote
+function tweetQuote(){
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+//Event listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //On load
 getQuote();
